a9261fb3ad58aae4dbadab736713d3248aba9373
Claudio Costa <claudio@imaginaction.com.br>
2019-10-07 17:34:57 -0300

    feet: 04 - 01 - Utilizando classes enum

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,11 +3,11 @@ package br.com.alura.financask.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.adapter.ListaTrasacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -17,13 +17,12 @@ class ListaTransacoesActivity : AppCompatActivity() {
         val transacoes = listOf(
             Transacao(
                 BigDecimal(20.5),
-                "Comida",
-                Calendar.getInstance()
+                Tipo.DESPESA
             ),
             Transacao(
                 BigDecimal(100.0),
                 "Economia",
-                Calendar.getInstance()
+                Tipo.RECEITA
             )
         )
         lista_transacoes_listview.adapter = ListaTrasacoesAdapter(this, transacoes)
