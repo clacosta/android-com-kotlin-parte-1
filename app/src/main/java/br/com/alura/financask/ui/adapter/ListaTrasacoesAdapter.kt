5adfc94ae6d6c50986dde1359eda720757a0fff2
Claudio Costa <claudio@imaginaction.com.br>
2019-10-07 19:29:39 -0300

    feet: 06 - 04 - Refatorando o Adapter

--- app/src/main/java/br/com/alura/financask/ui/adapter/ListaTrasacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financask/ui/adapter/ListaTrasacoesAdapter.kt
@@ -13,15 +13,12 @@ import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
 
-private val limiteDaCategoria = 14
-
 class ListaTrasacoesAdapter(
-    context: Context,
-    transacoes: List<Transacao>
+    private val context: Context,
+    private val transacoes: List<Transacao>
 ) : BaseAdapter() {
 
-    private val transacoes = transacoes
-    private val context = context
+    private val limiteDaCategoria: Int = 14
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val viewCriada = LayoutInflater.from(context).inflate(
@@ -29,35 +26,45 @@ class ListaTrasacoesAdapter(
             false
         )
         val transacao = transacoes[position]
-        if (transacao.tipo == Tipo.RECEITA) {
-            viewCriada.transacao_valor.setTextColor(
-                ContextCompat.getColor(
-                    context,
-                    R.color.receita
-                )
-            )
-        } else {
-            viewCriada.transacao_valor.setTextColor(
-                ContextCompat.getColor(
-                    context,
-                    R.color.despesa
-                )
-            )
-        }
-        if (transacao.tipo == Tipo.RECEITA) {
-            viewCriada.transacao_icone.setBackgroundResource(
-                R.drawable.icone_transacao_item_receita
-            )
-        } else {
-            viewCriada.transacao_icone.setBackgroundResource(
-                R.drawable.icone_transacao_item_despesa
-            )
-        }
 
-        viewCriada.transacao_valor.text = transacao.valor.formataParaBrasileiro()
-        viewCriada.transacao_categoria.text = transacao.categoria.limitaEmAte(limiteDaCategoria)
-        viewCriada.transacao_data.text = transacao.data.formataParaBrasileiro()
+        adicionaValor(transacao, viewCriada)
+        adicionaIcone(transacao, viewCriada)
+        adicionaCategoria(viewCriada, transacao)
+        adicionaData(viewCriada, transacao)
         return viewCriada
+    }
+
+    private fun adicionaData(viewCriada: View, transacao: Transacao) {
+        viewCriada.transacao_data.text = transacao.data.formataParaBrasileiro()
+    }
+
+    private fun adicionaCategoria(viewCriada: View, transacao: Transacao) {
+        viewCriada.transacao_categoria.text = transacao.categoria.limitaEmAte(limiteDaCategoria)
+    }
+
+    private fun adicionaIcone(transacao: Transacao, viewCriada: View) {
+        val icone: Int = iconePor(transacao.tipo)
+        viewCriada.transacao_icone.setBackgroundResource(icone)
+    }
+
+    private fun iconePor(tipo: Tipo): Int {
+        return when (tipo) {
+            Tipo.RECEITA -> R.drawable.icone_transacao_item_receita
+            else -> R.drawable.icone_transacao_item_despesa
+        }
+    }
+
+    private fun adicionaValor(transacao: Transacao, viewCriada: View) {
+        val cor: Int = corPor(transacao.tipo)
+        viewCriada.transacao_valor.setTextColor(cor)
+        viewCriada.transacao_valor.text = transacao.valor.formataParaBrasileiro()
+    }
+
+    private fun corPor(tipo: Tipo): Int {
+        return when (tipo) {
+            Tipo.RECEITA -> ContextCompat.getColor(context, R.color.receita)
+            else -> ContextCompat.getColor(context, R.color.despesa)
+        }
     }
 
     override fun getItem(position: Int): Transacao {
